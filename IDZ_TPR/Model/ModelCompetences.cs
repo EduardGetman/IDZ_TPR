e827c948df800d244347df1cf8b0a20b345df059
EduardGetman <getman.edik2016@yandex.ru>
2021-07-04 21:25:09 +0300

    Добавленны основные сущности и определенны их поля

---
+++ IDZ_TPR/Model/ModelCompetences.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDZ_TPR.Model
+{
+    class ModelCompetences
+    {
+        string name;
+        private List<Competence> competences;
+
+        public string Name { get => name; set => name = value; }
+        internal List<Competence> Competences { get => competences; set => competences = value; }
+    }
+}
---
+++ IDZ_TPR/Model/Position.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDZ_TPR.Model
+{
+    class Position: ModelCompetences
+    {
+        // Значимость выполняемой функции (w)
+        int significance;
+
+        public int Significance
+        {
+            get => significance; set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Значимость это положительное число");
+                }
+                significance = value;
+            }
+        }
+    }
+}
