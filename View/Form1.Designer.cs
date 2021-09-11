90c881e57c522a7e2850cc8ea6fd0f6610c950c9
Ustis <mihail.2000@bk.ru>
2021-09-11 17:40:54 +0300

    Создана форма, добавлены поля для ввода

---
+++ View/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.employeeSkillsDGV = new System.Windows.Forms.DataGridView();
+			this.necessarySkillsDGV = new System.Windows.Forms.DataGridView();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.calculateEmployeeAssignmentButton = new System.Windows.Forms.Button();
+			this.employeeCompetenceNUD = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.employeeCountNUD = new System.Windows.Forms.NumericUpDown();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.applyForEmployeeBTN = new System.Windows.Forms.Button();
+			this.functionCountNUD = new System.Windows.Forms.NumericUpDown();
+			this.competenceCountNUD = new System.Windows.Forms.NumericUpDown();
+			this.applySkillsNUD = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.employeeSkillsDGV)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.necessarySkillsDGV)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.employeeCompetenceNUD)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.employeeCountNUD)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.functionCountNUD)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.competenceCountNUD)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// employeeSkillsDGV
+			// 
+			this.employeeSkillsDGV.AllowUserToAddRows = false;
+			this.employeeSkillsDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.employeeSkillsDGV.Location = new System.Drawing.Point(12, 30);
+			this.employeeSkillsDGV.Name = "employeeSkillsDGV";
+			this.employeeSkillsDGV.Size = new System.Drawing.Size(698, 522);
+			this.employeeSkillsDGV.TabIndex = 0;
+			// 
+			// necessarySkillsDGV
+			// 
+			this.necessarySkillsDGV.AllowUserToAddRows = false;
+			this.necessarySkillsDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.necessarySkillsDGV.Location = new System.Drawing.Point(716, 30);
+			this.necessarySkillsDGV.Name = "necessarySkillsDGV";
+			this.necessarySkillsDGV.Size = new System.Drawing.Size(718, 522);
+			this.necessarySkillsDGV.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(225, 18);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Навыки сотрудников";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(713, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(230, 17);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Необходнимые навки";
+			// 
+			// calculateEmployeeAssignmentButton
+			// 
+			this.calculateEmployeeAssignmentButton.Location = new System.Drawing.Point(595, 659);
+			this.calculateEmployeeAssignmentButton.Name = "calculateEmployeeAssignmentButton";
+			this.calculateEmployeeAssignmentButton.Size = new System.Drawing.Size(229, 23);
+			this.calculateEmployeeAssignmentButton.TabIndex = 4;
+			this.calculateEmployeeAssignmentButton.Text = "Расчитать распределение сотрудников";
+			this.calculateEmployeeAssignmentButton.UseVisualStyleBackColor = true;
+			// 
+			// employeeCompetenceNUD
+			// 
+			this.employeeCompetenceNUD.Location = new System.Drawing.Point(510, 584);
+			this.employeeCompetenceNUD.Name = "employeeCompetenceNUD";
+			this.employeeCompetenceNUD.Size = new System.Drawing.Size(120, 20);
+			this.employeeCompetenceNUD.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(353, 560);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(140, 15);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Колличество сотрудников";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(353, 587);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(151, 15);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Колличество компетенций";
+			// 
+			// employeeCountNUD
+			// 
+			this.employeeCountNUD.Location = new System.Drawing.Point(510, 558);
+			this.employeeCountNUD.Name = "employeeCountNUD";
+			this.employeeCountNUD.Size = new System.Drawing.Size(120, 20);
+			this.employeeCountNUD.TabIndex = 8;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(781, 560);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(124, 15);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Количество функций";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(781, 587);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(138, 15);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Количество компетенций";
+			// 
+			// applyForEmployeeBTN
+			// 
+			this.applyForEmployeeBTN.Location = new System.Drawing.Point(483, 610);
+			this.applyForEmployeeBTN.Name = "applyForEmployeeBTN";
+			this.applyForEmployeeBTN.Size = new System.Drawing.Size(75, 23);
+			this.applyForEmployeeBTN.TabIndex = 13;
+			this.applyForEmployeeBTN.Text = "Применить";
+			this.applyForEmployeeBTN.UseVisualStyleBackColor = true;
+			this.applyForEmployeeBTN.Click += new System.EventHandler(this.applyForEmployeeBTN_Click);
+			// 
+			// functionCountNUD
+			// 
+			this.functionCountNUD.Location = new System.Drawing.Point(926, 558);
+			this.functionCountNUD.Name = "functionCountNUD";
+			this.functionCountNUD.Size = new System.Drawing.Size(120, 20);
+			this.functionCountNUD.TabIndex = 14;
+			// 
+			// competenceCountNUD
+			// 
+			this.competenceCountNUD.Location = new System.Drawing.Point(926, 585);
+			this.competenceCountNUD.Name = "competenceCountNUD";
+			this.competenceCountNUD.Size = new System.Drawing.Size(120, 20);
+			this.competenceCountNUD.TabIndex = 15;
+			// 
+			// applySkillsNUD
+			// 
+			this.applySkillsNUD.Location = new System.Drawing.Point(876, 610);
+			this.applySkillsNUD.Name = "applySkillsNUD";
+			this.applySkillsNUD.Size = new System.Drawing.Size(75, 23);
+			this.applySkillsNUD.TabIndex = 16;
+			this.applySkillsNUD.Text = "Применить";
+			this.applySkillsNUD.UseVisualStyleBackColor = true;
+			this.applySkillsNUD.Click += new System.EventHandler(this.applySkillsNUD_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1446, 690);
+			this.Controls.Add(this.applySkillsNUD);
+			this.Controls.Add(this.competenceCountNUD);
+			this.Controls.Add(this.functionCountNUD);
+			this.Controls.Add(this.applyForEmployeeBTN);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.employeeCountNUD);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.employeeCompetenceNUD);
+			this.Controls.Add(this.calculateEmployeeAssignmentButton);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.necessarySkillsDGV);
+			this.Controls.Add(this.employeeSkillsDGV);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.employeeSkillsDGV)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.necessarySkillsDGV)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.employeeCompetenceNUD)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.employeeCountNUD)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.functionCountNUD)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.competenceCountNUD)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.DataGridView employeeSkillsDGV;
+
+        private System.Windows.Forms.NumericUpDown employeeCompetenceNUD;
+
+        private System.Windows.Forms.NumericUpDown employeeCountNUD;
+
+        private System.Windows.Forms.Button applyForEmployeeBTN;
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+
+        private System.Windows.Forms.Button calculateEmployeeAssignmentButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.DataGridView employeeSkillsdataGridView;
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.DataGridView necessarySkillsDGV;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+		#endregion
+
+		private System.Windows.Forms.NumericUpDown functionCountNUD;
+		private System.Windows.Forms.NumericUpDown competenceCountNUD;
+		private System.Windows.Forms.Button applySkillsNUD;
+	}
+}
---
+++ View/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void applyForEmployeeBTN_Click(object sender, EventArgs e)
+        {
+            employeeSkillsDGV.Rows.Clear();
+            employeeSkillsDGV.Columns.Clear();
+
+            employeeSkillsDGV.Columns.Add("ФИО сотрудников", "ФИО сотрудников");
+            for (int i = 0; i < employeeCompetenceNUD.Value; ++i)
+            {
+                employeeSkillsDGV.Columns.Add(i.ToString(), "");
+            }
+
+			for (var i = 0; i <= employeeCountNUD.Value; ++i)
+			{
+				employeeSkillsDGV.Rows.Add();
+			}
+
+			employeeSkillsDGV.Rows[0].HeaderCell.Value = "Названия компетенций";
+    }
+
+    private void applySkillsNUD_Click(object sender, EventArgs e)
+		{
+            necessarySkillsDGV.Rows.Clear();
+            necessarySkillsDGV.Columns.Clear();
+
+            necessarySkillsDGV.Columns.Add("Функция", "Функция");
+            for (int i = 0; i < functionCountNUD.Value; ++i)
+            {
+                necessarySkillsDGV.Columns.Add(i.ToString(), "");
+            }
+
+            for (var i = 0; i <= competenceCountNUD.Value; ++i)
+            {
+                necessarySkillsDGV.Rows.Add();
+            }
+
+            necessarySkillsDGV.Rows[0].HeaderCell.Value = "Названия компетенций";
+        }
+	}
+}
