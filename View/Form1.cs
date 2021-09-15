d7af8df837a67b48da79496820883253cc195f65
Ustis <mihail.2000@bk.ru>
2021-09-15 17:34:18 +0300

    Добавлено получения информации из  Form1

--- View/Form1.cs
+++ View/Form1.cs
@@ -32,16 +32,16 @@ namespace View
                 employeeSkillsDGV.Columns.Add(i.ToString(), "");
             }
 
-			for (var i = 0; i <= employeeCountNUD.Value; ++i)
-			{
-				employeeSkillsDGV.Rows.Add();
-			}
+            for (var i = 0; i <= employeeCountNUD.Value; ++i)
+            {
+                employeeSkillsDGV.Rows.Add();
+            }
 
-			employeeSkillsDGV.Rows[0].HeaderCell.Value = "Названия компетенций";
+            employeeSkillsDGV.Rows[0].HeaderCell.Value = "Названия компетенций";
         }
 
-    private void applySkillsNUD_Click(object sender, EventArgs e)
-		{
+        private void applySkillsNUD_Click(object sender, EventArgs e)
+        {
             necessarySkillsDGV.Rows.Clear();
             necessarySkillsDGV.Columns.Clear();
 
@@ -58,20 +58,58 @@ namespace View
 
             necessarySkillsDGV.Rows[0].HeaderCell.Value = "Названия компетенций";
 
-			//employeeSkillsDGV.Rows.Add();
-			//employeeSkillsDGV.Rows.Add();
+            necessarySkillsDGV.Columns.Add("Коэффициент важности производственной функции", "Коэффициент важности производственной функции");
 
-			//employeeSkillsDGV.Rows[Convert.ToInt32(competenceCountNUD.Value) + 1].HeaderCell.Value = "Минимальный навык";
-			//employeeSkillsDGV.Rows[Convert.ToInt32(competenceCountNUD.Value) + 2].HeaderCell.Value = "Максимальный навык";
-		}
+            //employeeSkillsDGV.Rows.Add();
+            //employeeSkillsDGV.Rows.Add();
 
-		private void calculateEmployeeAssignmentButton_Click(object sender, EventArgs e)
-		{
-		}
+            //employeeSkillsDGV.Rows[Convert.ToInt32(competenceCountNUD.Value) + 1].HeaderCell.Value = "Минимальный навык";
+            //employeeSkillsDGV.Rows[Convert.ToInt32(competenceCountNUD.Value) + 2].HeaderCell.Value = "Максимальный навык";
+        }
 
-		private void maxSkillLevelNUD_ValueChanged(object sender, EventArgs e)
-		{
+        private void calculateEmployeeAssignmentButton_Click(object sender, EventArgs e)
+        {
+        }
 
-		}
-	}
+        private void maxSkillLevelNUD_ValueChanged(object sender, EventArgs e)
+        {
+            List<String> skillNames = new List<string>();
+            for (int i = 1; i < necessarySkillsDGV.Columns.Count; ++i)
+            {
+                skillNames.Add(necessarySkillsDGV.Rows[0].Cells[i].Value.ToString());
+            }
+
+            int[,] positionsLevels = new int[necessarySkillsDGV.Rows.Count - 1, necessarySkillsDGV.Columns.Count - 1];
+            List<List<int>> positionsLevelss = new List<List<int>>();
+            for (int i = 1; i <= necessarySkillsDGV.Rows.Count; ++i)
+            {
+                for (int j = 1; j <= necessarySkillsDGV.Columns.Count - 1; ++j)
+                {
+                    positionsLevels[i - 1, j - 1] = Convert.ToInt32(necessarySkillsDGV.Rows[i].Cells[j].Value);
+                }
+            }
+
+            int[] importanceCoefficient = new int[necessarySkillsDGV.Columns.Count - 1];
+            for (int i = 1; i < necessarySkillsDGV.Rows.Count; ++i)
+            {
+                importanceCoefficient[i - 1] = Convert.ToInt32(necessarySkillsDGV.Rows[i].Cells[necessarySkillsDGV.Columns.Count - 1].Value);
+            }
+
+
+            int[,] employsLevels = new int[employeeSkillsDGV.Rows.Count, employeeSkillsDGV.Columns.Count];
+
+            for (int i = 1; i <= employeeSkillsDGV.Rows.Count; ++i)
+            {
+                for (int j = 1; j <= employeeSkillsDGV.Columns.Count; ++j)
+                {
+                    employsLevels[i - 1, j - 1] = Convert.ToInt32(employeeSkillsDGV.Rows[i].Cells[j].Value);
+
+                }
+            }
+
+            int scaleMin = Convert.ToInt32(minSkillLevelNUD.Value);
+            int scaleMax = Convert.ToInt32(maxSkillLevelNUD.Value);
+
+        }
+    }
 }
