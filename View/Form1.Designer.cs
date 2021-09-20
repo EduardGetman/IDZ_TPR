bfd07e8698b786b1523f2a29350c438b9b5c6807
Ustis <mihail.2000@bk.ru>
2021-09-20 22:16:18 +0300

    Исправил кнопки

--- View/Form1.Designer.cs
+++ View/Form1.Designer.cs
@@ -222,7 +222,7 @@
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(75, 23);
 			this.button3.TabIndex = 22;
-			this.button3.Text = "button3";
+			this.button3.Text = "Пример1";
 			this.button3.UseVisualStyleBackColor = true;
 			this.button3.Click += new System.EventHandler(this.button3_Click);
 			// 
@@ -232,7 +232,7 @@
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(75, 23);
 			this.button2.TabIndex = 23;
-			this.button2.Text = "button2";
+			this.button2.Text = "Пример2";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.button2_Click);
 			// 
